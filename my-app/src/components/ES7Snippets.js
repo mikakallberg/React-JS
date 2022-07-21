9b8ff6430bf50793d85ccadcee9694d875242f3e
mikakallberg <mikaela.kallberg@hotmail.com>
2022-07-21 15:34:10 +0000

    ES/ and abbreviations

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+// Get by typing rfce
+// function ES7Snippets() {
+//     return (
+//     <div>ES7Snippets</div>
+//     )
+// }
+
+// export default ES7Snippets
+
+//Get by typing rafce
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//     return (
+//     <div>ES7Snippets</div>
+//     )
+// }
+
+// export default ES7Snippets
+
+//Get by typing rce
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//     render() {
+//     return (
+//         <div>ES7Snippets</div>
+//     )
+//     }
+// }
+
+// export default ES7Snippets
+
+// Get by typing rconst
+// constructor(props) {
+//     super(props)
+
+//     this.state = {
+//         first
+//     }
+// }
+
+// Get by typing clg
+// console.log(first)
+
+// Get by typing clo
+// console.log('first', first)
