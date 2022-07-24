92dfa03386b1b65e0da123b340f60cbf6771b8bb
mikakallberg <mikaela.kallberg@hotmail.com>
2022-07-24 17:45:04 +0000

    component lifecycle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
-import RenderingList from './components/RenderingList';
-// import MethodsAsPropsParents from './components/MethodsAsPropsParents';
-// import NestingComponents from './components/NestingComponents';
-// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-// import EventBindings from './components/EventBinding';
-// import EventsClass from './components/EventsClass';
-// import EventsFunctional from './components/EventsFunctional';
-// import FunctionalGreetingWitProps from './components/FunctionalGreetingWithProps';
-// import StatefulGreeting from './components/StatefulGreeting';
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
+
 
 function App() {
   return (
     <div className="App">
-      <RenderingList/>
-      {/* <MethodsAsPropsParents/> */
-      /* <NestingComponents/> */
-      /* <ConditionalRenderingClass/>
-      <ConditionalRenderingFunctional connected={true}/> */
-      /* <EventBindings/> */
-      /* <EventsFunctional/>
-      <EventsClass/> */
-      /* <FunctionalGreetingWitProps greeting="nice to meet you!" name="Mike" age="32" />
-      <StatefulGreeting greeting="I'm a stateful class component!" name="Lars"/> */}
+      <LifeCyclesCWU />
     </div>
   );
 }
