c9cccb862d11a014087b1c55cbaebbdda2c8ee46
mikakallberg <mikaela.kallberg@hotmail.com>
2022-07-28 09:15:09 +0000

    hooks with toggle

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 
-// Running effects only once
+// Clean up effect
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
     const [time, setTime] = useState(0);
@@ -9,14 +9,17 @@ function UseEffectCounter() {
         console.log('count 1 effect');
         document.title = count;
     }, [count]);
-
     useEffect(()=>{
         console.log('Creating timer');
         const interval = setInterval(() => {
+            console.log('Interval executed');
             setTime(time => time + 1)
         }, 1000);
+        return () => {
+            console.log('cleaning up!');
+            clearInterval(interval);
+        }
     }, []);
-    
     return (
         <div>
             <button onClick={
@@ -28,6 +31,35 @@ function UseEffectCounter() {
         </div>
     )
 }
+
+// Running effects only once
+// function UseEffectCounter() {
+//     const [count, setCount] = useState(0);
+//     const [time, setTime] = useState(0);
+
+//     useEffect(() => {
+//         console.log('count 1 effect');
+//         document.title = count;
+//     }, [count]);
+
+//     useEffect(()=>{
+//         console.log('Creating timer');
+//         const interval = setInterval(() => {
+//             setTime(time => time + 1)
+//         }, 1000);
+//     }, []);
+    
+//     return (
+//         <div>
+//             <button onClick={
+//                 () => setCount(count => count + 1)
+//             }>
+//                 Increment Count ({count})
+//             </button>
+//             <h2>Time is {time}</h2>
+//         </div>
+//     )
+// }
 
 
 // Conditional effects
